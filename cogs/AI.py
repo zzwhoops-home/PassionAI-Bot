04126c1f48819df2bd364a3d80be16ef5d1ae7f4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 23:54:17 -0500

    Add 2 chatbot versions

--- cogs/AI.py
+++ cogs/AI.py
@@ -22,25 +22,28 @@ class AI(commands.Cog):
         if (length == -1):
             await ctx.channel.send(f"{ctx.author.mention} Please enter something you want to ask me.")
             return
-        if (length > 100):
-            await ctx.channel.send(f"{ctx.author.mention} Hey, try to keep your requests under 100 characters.")
+        if (length > 200):
+            await ctx.channel.send(f"{ctx.author.mention} Hey, try to keep your requests under 200 characters.")
             return
 
-        max_tokens = 500
+        max_tokens = 150
+        prompt = "You are a friendly chatbot.\n"
 
         # customize response
         response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message,
-        temperature=0.5,
-        max_tokens=max_tokens
+            model="text-davinci-003",
+            prompt=f"{prompt} {message}",
+            temperature=0,
+            max_tokens=max_tokens
         )
         
         # convert to json, extract text with no new lines
         response_json = json.loads(str((response)))
         text = response_json['choices'][0]['text'].strip()
 
-        await ctx.channel.send(f"{ctx.author.mention} {text}")
+        embed = nextcord.Embed(description=text)
+
+        await ctx.channel.send(f"{ctx.author.mention}", embed=embed)
 
         usage = int(response_json['usage']['total_tokens'])
         
@@ -48,3 +51,42 @@ class AI(commands.Cog):
         if (usage >= max_tokens):
             await ctx.channel.send(f"{ctx.author.mention} Hey, this response may be cut off due to API limitations!")
             return
+
+    @commands.command(name="chattm", aliases=["ctm"])
+    async def chat_trained_model(self, ctx, *message: str):
+        length = -1
+        for word in message:
+            length += len(word) + 1
+        if (length == -1):
+            await ctx.channel.send(f"{ctx.author.mention} Please enter something you want to ask me.")
+            return
+        if (length > 200):
+            await ctx.channel.send(f"{ctx.author.mention} Hey, try to keep your requests under 200 characters.")
+            return
+
+        max_tokens = 150
+        prompt = "You are a friendly chatbot.\n"
+
+        # customize response
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"{prompt} {message}",
+            temperature=0,
+            max_tokens=max_tokens
+        )
+        
+        # convert to json, extract text with no new lines
+        response_json = json.loads(str((response)))
+        text = response_json['choices'][0]['text'].strip()
+
+        embed = nextcord.Embed(description=text)
+
+        await ctx.channel.send(f"{ctx.author.mention}", embed=embed)
+
+        usage = int(response_json['usage']['total_tokens'])
+        
+        # warn user if AI response may be cut off
+        if (usage >= max_tokens):
+            await ctx.channel.send(f"{ctx.author.mention} Hey, this response may be cut off due to API limitations!")
+            return
+
