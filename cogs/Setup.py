69e2823fa21681aa2ef682b7bda63004b7e4c690
zzwhoops-home <zd@5rock.org>
2024-02-15 23:32:01 -0500

    Add reaction --> private channel function (WIP)

--- cogs/Setup.py
+++ cogs/Setup.py
@@ -49,25 +49,31 @@ class Setup(commands.Cog):
             await setup_msg.delete()
             await verify_channel_msg.delete()
 
-        # ensure reaction is valid, and that the reacting user is the author of the setup message
+        # ensure reaction is valid, and that the reacting user is the author of the setup message, and that the reaction is on the correct message
         def check_message(user_response, user):
-            return user == ctx.author and str(user_response.emoji) in ["✅", "❌"]
+            return user == ctx.author and str(user_response.emoji) in ["✅", "❌"] and user_response.message.id == verify_channel_msg.id
         try:
             user_response, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check_message)
             reaction_str = str(user_response.emoji)
 
+            # continue to run function that sets up the server
             if reaction_str == "✅":
                 await ctx.channel.send(f"Setting up in <#{ctx.channel.id}>...", delete_after=5.0)
                 await self.setup_verified(ctx)
+
+            # go to cancel function
             elif reaction_str == "❌":
                 await ctx.channel.send(f"Canceling!", delete_after=3.0)
                 await cancel()
+                return
         except asyncio.TimeoutError:
+            # go to cancel function
             await ctx.channel.send(f"{ctx.author.mention} You didn't react in time. Please run the command again.", delete_after=10.0)
             await cancel()
-        except Exception as e:
-            await ctx.channel.send(f"An error occurred: {e}")
             return
+        # except Exception as e:
+        #     await ctx.channel.send(f"An error occurred: {e}")
+        #     return
         
     async def check_already_setup(self, ctx):
         """Checks if the server specified in the given context has already gone through the PassionAI setup process
@@ -88,7 +94,7 @@ class Setup(commands.Cog):
         # returns if the document was found (guild was already setup)
         return result is not None
 
-    async def setup_verified(self, ctx):
+    async def setup_verified(self, ctx: nextcord.ext.commands.Context):
         """
         Runs after the user verifies that they want to setup the bot in this channel.
         """
@@ -105,17 +111,38 @@ class Setup(commands.Cog):
         # set permissions for channel
         await ctx.channel.set_permissions(default_role, overwrite=permissions)
 
+        # create category for AI
+        ai_category = await ctx.guild.create_category("AI Channels")
+
+        # add to DB
+        await self.setup_verified_update_DB(ctx, ai_category.id)
+
+        # welcome user and add reaction
         welcome_msg = await ctx.channel.send(f"Welcome to PassionAI! Please click the below reaction to create a private channel for yourself.")
         await welcome_msg.add_reaction("🍊")
 
-        # add to DB
-        await self.setup_verified_update_DB(ctx)
+        # ensure reaction is valid, and that the reacting user is the author of the setup message
+        # REMEMBER TO CHECK CORRECT MESSAGE ID - needs to come from DB
+        def check_message(user_response, user):
+            return str(user_response.emoji) == "🍊" and user_response.message.id == welcome_msg.id and user.id != self.bot.user.id
+        
+        user_response, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check_message)
+        reaction_str = str(user_response.emoji)
 
-    async def setup_verified_update_DB(self, ctx):
+        # unnecessary right now, may be useful later
+        if reaction_str == "🍊":
+            await self.create_private_channel(user_response.message.guild, user)
+
+
+    async def setup_verified_update_DB(self, ctx: nextcord.ext.commands.Context, category_id: int):
+        
+        # awwoo 
         """Once the user finishes setting up in a channel, update our database with the current guild's ID
         Args:
             ctx (nextcord.ext.commands.Context): Context
+            category_id (int): The category ID for AI channels
         """
+
         # get guild id and name from context
         guild_id = ctx.guild.id
         guild_name = ctx.guild.name
@@ -129,7 +156,8 @@ class Setup(commands.Cog):
         data = {
             "$setOnInsert": {
                 "timestamp": datetime.now(),
-                "guild_id": guild_id
+                "guild_id": guild_id,
+                "category_id": category_id
             },
             "$set": {
                 "guild_name": guild_name
@@ -137,6 +165,36 @@ class Setup(commands.Cog):
         }
 
         self.bot.guilds_setup.find_one_and_update(filter=query, update=data, upsert=True)
+
+    async def create_private_channel(self, guild: nextcord.Guild, user: nextcord.Member):
+        """
+        Creates a private channel for a user to interact privately with PassionAI
+            guild (nextcord.Guild): The guild to create the channel in
+            user (nextcord.Member): The user who reacted with the setup message
+        """
+        # get guild default role (everyone)
+        default_role = guild.default_role
+
+        channel_name = f"{user.name}-AI"
+        permissions = {
+            default_role: PermissionOverwrite(view_channel=False),
+            user: PermissionOverwrite(view_channel=True)
+        }
+
+        # get category ID of guild
+        query = {
+            "guild_id": guild.id
+        }
+        category_id = self.bot.guilds_setup.find_one(query)['category_id']
+        category = nextcord.utils.get(guild.categories, id=category_id)
+
+        channel = await guild.create_text_channel(
+            name = channel_name,
+            overwrites = permissions,
+            category = category
+        )
+
+        await channel.send(f"{user.mention} This is your private channel. Do note that server administrators, including the PQLife team, will be able to see your questions and PassionAI's responses.")
 
 def setup(bot):
     bot.add_cog(Setup(bot))
