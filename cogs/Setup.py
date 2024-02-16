b57c193e77c1df105bdfa2fe721ef11f25ce5dbf
zzwhoops-home <zd@5rock.org>
2024-02-15 22:30:03 -0500

    Basic setup code - prompting user to set up channel

--- cogs/Setup.py
+++ cogs/Setup.py
@@ -1,7 +1,9 @@
 import nextcord
+from nextcord import PermissionOverwrite
 from nextcord.ext import commands
 
 import os
+import asyncio
 from openai import OpenAI
 
 import pymongo
@@ -9,6 +11,78 @@ import pymongo
 class Setup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name="setup")
+    @commands.has_permissions(administrator=True)
+    async def setup(self, ctx):
+        """
+        Command that handles the setup of the bot in a new server.
+        The bot should have administrator privileges for this to work without issues.
+        Args:
+            ctx (nextcord.ext.commands.Context): Context
+        Returns:
+            _type_: _description_
+        """
+
+        # delete original command message
+        await ctx.message.delete()
+
+        # tell the user we are setting up in this channel
+        setup_msg = await ctx.channel.send(f"{ctx.author.mention} You are now setting up **PassionAI** for **{ctx.guild.name}**. Please follow the prompts to proceed.")
+
+        # get the user to confirm, adding two reactions
+        verify_channel_msg = await ctx.channel.send(f"This is channel <#{ctx.channel.id}>. Are you sure you want to use this channel as your setup channel? The channel's permissions will be **irrevocably altered**! It is also advised to set up in a blank channel.")
+        await verify_channel_msg.add_reaction("✅")
+        await verify_channel_msg.add_reaction("❌")
+
+        async def cancel():
+            """
+            Within setup: deletes messages used to setup the bot in the channel
+            """
+            await setup_msg.delete()
+            await verify_channel_msg.delete()
+
+        # ensure reaction is valid, and that the reacting user is the author of the setup message
+        def check_message(user_response, user):
+            return user == ctx.author and str(user_response.emoji) in ["✅", "❌"]
+        try:
+            user_response, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check_message)
+            reaction_str = str(user_response.emoji)
+
+            if reaction_str == "✅":
+                await ctx.channel.send(f"Setting up in <#{ctx.channel.id}>...", delete_after=5.0)
+                await self.setup_verified(ctx)
+            elif reaction_str == "❌":
+                await ctx.channel.send(f"Canceling!", delete_after=3.0)
+                await cancel()
+        except asyncio.TimeoutError:
+            await ctx.channel.send(f"{ctx.author.mention} You didn't react in time. Please run the command again.", delete_after=10.0)
+            await cancel()
+        except Exception as e:
+            await ctx.channel.send(f"An error occurred: {e}")
+            return
+        
+    async def setup_verified(self, ctx):
+        """
+        Runs after the user verifies that they want to setup the bot in this channel.
+        Returns:
+            _type_: _description_
+        """
+        # get default role
+        default_role = ctx.guild.default_role
+
+        # remove any channel permission overwrites
+        for overwrite in ctx.channel.overwrites:
+            await ctx.channel.set_permissions(overwrite, overwrite=None)
+
+        # remove member reaction and message permissions
+        permissions = PermissionOverwrite(send_messages=False, add_reactions=False)
+
+        # set permissions for channel
+        await ctx.channel.set_permissions(default_role, overwrite=permissions)
+
+        welcome_msg = await ctx.channel.send(f"Welcome to PassionAI! Please click the below reaction to create a private channel for yourself.")
+        await welcome_msg.add_reaction("🍊")
 
 def setup(bot):
     bot.add_cog(Setup(bot))
